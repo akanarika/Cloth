989bfb10671bae99baedcffb10ab9f51708d2e65
Chaoying Du <chaoyind@andrew.cmu.edu>
2017-03-10 17:03:29 -0500

    cloth init

--- main.cpp
+++ main.cpp
@@ -15,15 +15,27 @@
 #include <GL/glew.h>
 // GLFW
 #include <GLFW/glfw3.h>
+// Glm
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 // Shader loading
 #include "shader.h"
 
+// Key Callback
 void key_callback(GLFWwindow* window, int key, int scancode, 
                   int action, int mode);
 
+// Window
 GLFWwindow* window;
-int row_count = 10;
-int col_count = 10;
+
+// Window size
+const GLuint WIDTH = 800, HEIGHT = 600;
+
+// Cloth attribute
+float cloth_size = 10.0f;
+int row_count = (int)cloth_size + 1;
+int col_count = (int)cloth_size + 1;
 int vertex_count = row_count * col_count;
 
 int main() {
@@ -38,7 +50,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // Important in Mac
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
     
-    window = glfwCreateWindow(800, 600, "Cloth", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Cloth", NULL, NULL);
     if (window == NULL)
     {
             fprintf(stderr, "Failed to create GLFW window\n");
@@ -49,25 +61,30 @@ int main() {
     glfwSetKeyCallback(window, key_callback);
     
     glewExperimental = GL_TRUE;
+
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
         return -1;
     }
 
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);
-    glViewport(0, 0, width, height);
+    // Viewport
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    // Depth test
+    glEnable(GL_DEPTH_TEST);
 
     // Shader reading
-    Shader myShader("vs.glsl", "fs.glsl");
+    Shader shader_program("vs.glsl", "fs.glsl");
     
     GLfloat vertices[vertex_count*3];
     // Vertices list
     for (int i=0; i<row_count; i++) {
+        int stride = i * col_count * 3;
+        int grid_size = cloth_size / (col_count-1);
         for (int j=0; j<col_count; j++) {
-            vertices[i*col_count*3 + j*3] = 0.1f * i;
-            vertices[i*col_count*3 + j*3 + 1] = 0.1f * j;
-            vertices[i*col_count*3 + j*3 + 2] = 0.0f;
+            vertices[stride + j*3] = grid_size * i;
+            vertices[stride + j*3 + 1] = grid_size * j;
+            vertices[stride + j*3 + 2] = 0.0f;
         }
     }
     std::cout << "count: " << vertex_count << std::endl;
@@ -76,14 +93,15 @@ int main() {
     GLuint indices[index_count];
     // Index list
     for (int i=0; i<row_count-1; i++) {
+        int stride = i * (col_count-1) * 6; 
         for (int j=0; j<col_count-1; j++) {
-            indices[i*(col_count-1)*6 + j*6] = i * col_count + j;
-            indices[i*(col_count-1)*6 + j*6 + 1] = i * col_count + j + 1;
-            indices[i*(col_count-1)*6 + j*6 + 2] = (i + 1) * col_count + j;
+            indices[stride + j*6] = i * col_count + j;
+            indices[stride + j*6 + 1] = i * col_count + j + 1;
+            indices[stride + j*6 + 2] = (i + 1) * col_count + j;
 
-            indices[i*(col_count-1)*6 + j*6 + 3] = i * col_count + j + 1;
-            indices[i*(col_count-1)*6 + j*6 + 4] = (i + 1) * col_count + j;
-            indices[i*(col_count-1)*6 + j*6 + 5] = (i + 1) * col_count + j + 1;
+            indices[stride + j*6 + 3] = i * col_count + j + 1;
+            indices[stride + j*6 + 4] = (i + 1) * col_count + j;
+            indices[stride + j*6 + 5] = (i + 1) * col_count + j + 1;
 
         }
     }
@@ -110,16 +128,42 @@ int main() {
     // Some attributes
     glPointSize(5);
     glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    
+
+        
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         // Render
         glClearColor(.2f, .3f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
         // Draw
-        myShader.Use();
+        shader_program.Use();
+        
+        // Perspective projection
+        glm::mat4 projection = glm::perspective(glm::radians(25.0f), 
+                                                (float)WIDTH/(float)HEIGHT, 
+                                                0.1f, 100.0f);
+		// Camera matrix
+		glm::mat4 view = glm::lookAt(
+			glm::vec3(0, 0, 3),
+		    glm::vec3(0, 0, 0),
+            glm::vec3(0, 1, 0)
+        );
+        // Model matrix
+        glm::mat4 model;
+        float scale = 0.5f;
+        model = glm::scale(model, glm::vec3(scale * 1.0f/cloth_size, 
+                                            scale * 1.0f/cloth_size, 
+                                            0.1f));
+        model = glm::translate(model, glm::vec3(-cloth_size/2.0f, 
+                                                -cloth_size/2.0f, 0.0f));
+
+        // Put transformation matrics together
+        glm::mat4 mvp = projection * view * model;
+        GLint mvp_loc = glGetUniformLocation(shader_program.Program, "mvp");
+        glUniformMatrix4fv(mvp_loc, 1, GL_FALSE, glm::value_ptr(mvp));
+
         glBindVertexArray(VAO);
         //glDrawArrays(GL_POINTS, 0, vertex_count*3);
         glDrawElements(GL_TRIANGLES, index_count, GL_UNSIGNED_INT, 0);
--- vs.glsl
+++ vs.glsl
@@ -2,9 +2,11 @@
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
 
+uniform mat4 mvp;
+
 out vec3 out_color;
 
 void main() {
-    gl_Position = vec4(position, 1.0);
-    out_color = color;
+    gl_Position = mvp * vec4(position, 1.0);
+    out_color = vec3(1.0f, 0.5f, 0.2f);
 }
